9b3a09f2e602751fdff1c2eb5efbf523af82821c
Hunter <muratoreh@gmail.com>
2024-02-26 14:44:29 -0500

    Add client side mutations and queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -19,3 +19,54 @@ export const LOGIN = gql`
     }
   }
 `
+
+// Add a whiskey to a user's collection
+export const ADD_TO_COLLECTION = gql`
+mutation AddToCollection($userId: ID!, $whiskeyId: ID!, $rating: Float, $notes: WhiskeyNotesInput) {
+  addToCollection(userId: $userId, whiskeyId: $whiskeyId, rating: $rating, notes: $notes) {
+    _id
+    userId
+    whiskeyId
+    rating
+    notes {
+      nose
+      taste
+      finish
+    }
+  }
+}
+`
+
+// Update a whiskey in a user's collection
+export const UPDATE_REVIEW = gql`
+mutation UpdateReview($userId: ID!, $whiskeyId: ID!, $rating: Float, $notes: WhiskeyNotesInput) {
+  updateReview(userId: $userId, whiskeyId: $whiskeyId, rating: $rating, notes: $notes) {
+    _id
+    userId
+    whiskeyId
+    rating
+    notes {
+      nose
+      taste
+      finish
+    }
+  }
+}
+`
+
+// Remove a whiskey from a user's collection
+export const REMOVE_FROM_COLLECTION = gql`
+mutation RemoveFromCollection($userId: ID!, $whiskeyId: ID!) {
+  removeFromCollection(userId: $userId, whiskeyId: $whiskeyId) {
+    _id
+    userId
+    whiskeyId
+    rating
+    notes {
+      nose
+      taste
+      finish
+    }
+  }
+}
+`
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -8,4 +8,56 @@ export const AUTHENTICATE = gql`
       username
     }
   }
-`;
+`
+
+export const GET_USER_BY_ID = gql`
+  query GetUserById($userId: ID!) {
+    getUserById(userId: $userId) {
+      _id
+      email
+      username
+    }
+  }
+`
+
+// Get whiskey based off search or get all whiskeys if no search
+export const GET_WHISKEYS = gql`
+  query GetWhiskeys($search: String) {
+    whiskeys(search: $search) {
+      _id
+      name
+      img
+      type
+      distiller
+      country
+      region
+      bottler
+      abv
+      age
+      price
+      notes {
+        nose
+        taste
+        finish
+      }
+      score
+    }
+  }
+`
+
+// Get a specific user's whiskey collection
+export const GET_USER_COLLECTION = gql`
+  query GetUserCollection($userId: ID!) {
+    getUserCollection(userId: $userId) {
+      _id
+      userId
+      whiskeyId
+      rating
+      notes {
+        nose
+        taste
+        finish
+      }
+    }
+  }
+`
