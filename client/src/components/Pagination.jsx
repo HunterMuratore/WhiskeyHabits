ea68b2246df0d90ef3c3921b0b656e012ebc641b
Hunter <muratoreh@gmail.com>
2024-03-05 12:53:21 -0500

    Update pagination controls and add button to clear table search

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,25 +1,58 @@
+import React from 'react';
+
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+    const pageNumbers = [];
+
+    // Generate page numbers
+    for (let i = 1; i <= totalPages; i++) {
+        pageNumbers.push(i);
+    }
+
     return (
-      <div className="flex justify-center mt-4">
-        <button
-          className="mr-2 px-4 py-2 bg-gray-200 text-gray-800 rounded-md"
-          onClick={() => onPageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          Previous
-        </button>
-        <span className="px-4 py-2 bg-gray-200 text-gray-800 rounded-md">
-          Page {currentPage} of {totalPages}
-        </span>
-        <button
-          className="ml-2 px-4 py-2 bg-gray-200 text-gray-800 rounded-md"
-          onClick={() => onPageChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
-        >
-          Next
-        </button>
-      </div>
-    )
-  }
-  
-  export default Pagination
+        <div className="pagination flex justify-center mx-auto mt-4">
+            <button
+                className="mr-2 my-btn"
+                onClick={() => onPageChange(currentPage - 1)}
+                disabled={currentPage === 1}
+            >
+                Previous
+            </button>
+            <button
+                className="mx-2"
+                onClick={() => onPageChange(1)}
+                disabled={currentPage === 1}
+            >
+                &lt;&lt;&lt;
+            </button>
+            {pageNumbers.slice(currentPage - 1, currentPage + 2).map((pageNumber) => (
+                <button
+                    key={pageNumber}
+                    className={`mx-1 px-3 py-2 ${
+                        pageNumber === currentPage ? 'font-bold' : ''
+                    }`}
+                    onClick={() => onPageChange(pageNumber)}
+                >
+                    {pageNumber}
+                </button>
+            ))}
+            {currentPage + 2 < totalPages && <span className="mx-2">...</span>}
+            {currentPage + 2 < totalPages && (
+                <button
+                    className="mx-2"
+                    onClick={() => onPageChange(totalPages)}
+                >
+                    {totalPages}
+                </button>
+            )}
+            <button
+                className="ml-2 my-btn"
+                onClick={() => onPageChange(currentPage + 1)}
+                disabled={currentPage === totalPages}
+            >
+                Next
+            </button>
+        </div>
+    );
+};
+
+export default Pagination;
