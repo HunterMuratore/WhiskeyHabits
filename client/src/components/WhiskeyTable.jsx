cce0a96241eb4dafe9c195d848d8a664498f0bbc
Hunter <muratoreh@gmail.com>
2024-03-25 12:01:08 -0400

    Added styling for table columns on small and medium screens

--- client/src/components/WhiskeyTable.jsx
+++ client/src/components/WhiskeyTable.jsx
@@ -7,19 +7,20 @@ import WhiskeyItem from './WhiskeyItem'
 
 const WhiskeyTable = ({ data, sortByName, sortByScore, handleSortByName, handleSortByScore, perPage }) => {
   const isSmallScreen = useMediaQuery({ maxWidth: 800 })
+  const isMedScreen = useMediaQuery({ maxWidth: 1000 })
 
   return (
     <>
       {isSmallScreen ? (
         <table className="whiskey-table table-auto min-w-full divide-y divide-gray-200">
-          {/* <colgroup>
-            <col style={{ width: '40%' }} />
-            <col style={{ width: '30%' }} />
-            <col style={{ width: '30%' }} />
-          </colgroup> */}
+          <colgroup>
+            <col style={{ width: '50%' }} />
+            <col style={{ width: '25%' }} />
+            <col style={{ width: '25%' }} />
+          </colgroup>
           <thead>
             <tr>
-              <th className="px-6 py-3 w-1/2 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              <th className="whitespace-nowrap px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                 Name
                 <FontAwesomeIcon
                   icon={sortByName === 'asc' ? faCaretUp : faCaretDown}
@@ -27,10 +28,10 @@ const WhiskeyTable = ({ data, sortByName, sortByScore, handleSortByName, handleS
                   className="ml-1 cursor-pointer"
                 />
               </th>
-              <th className="px-6 py-3 w-1/4 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              <th className="px-6 py-3 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                 Type
               </th>
-              <th className="px-6 py-3 w-1/4 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              <th className="whitespace-nowrap px-6 py-3 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                 Score
                 <FontAwesomeIcon
                   icon={sortByScore === 'highToLow' ? faCaretUp : faCaretDown}
@@ -43,13 +44,13 @@ const WhiskeyTable = ({ data, sortByName, sortByScore, handleSortByName, handleS
           <tbody className="bg-white divide-y divide-gray-200">
             {data.whiskeys.whiskeys.slice(0, perPage).map((whiskey, index) => (
               <tr key={whiskey._id}>
-                <td className="px-3 py-2 whitespace-nowrap text-sm font-medium text-gray-900">
+                <td className="px-3 py-2 whitespace-wrap text-sm font-medium text-gray-900">
                   {whiskey.name}
                 </td>
-                <td className="px-3 py-2 whitespace-nowrap text-sm text-gray-900">
+                <td className="px-3 py-2 whitespace-wrap text-sm text-center text-gray-900">
                   {whiskey.type}
                 </td>
-                <td className="px-3 py-2 whitespace-nowrap text-sm text-gray-900">
+                <td className="px-3 py-2 whitespace-wrap text-center text-sm text-gray-900">
                   {whiskey.rating}
                 </td>
               </tr>
@@ -58,9 +59,28 @@ const WhiskeyTable = ({ data, sortByName, sortByScore, handleSortByName, handleS
         </table>
       ) : (
         <table className="whiskey-table table-auto min-w-full divide-y divide-gray-200">
+          {isMedScreen ? (
+            <colgroup>
+              <col style={{ width: '35%' }} />
+              <col style={{ width: '25%' }} />
+              <col style={{ width: '20%' }} />
+              <col style={{ width: '10%' }} />
+              <col style={{ width: '5%' }} />
+              <col style={{ width: '5%' }} />
+            </colgroup>
+          ) : (
+            <colgroup>
+              <col style={{ width: '30%' }} />
+              <col style={{ width: '10%' }} />
+              <col style={{ width: '15%' }} />
+              <col style={{ width: '15%' }} />
+              <col style={{ width: '15%' }} />
+              <col style={{ width: '5%' }} />
+            </colgroup>
+          )}
           <thead>
             <tr>
-              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              <th className="whitespace-nowrap px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                 Name
                 <FontAwesomeIcon
                   icon={sortByName === 'asc' ? faCaretUp : faCaretDown}
@@ -80,7 +100,7 @@ const WhiskeyTable = ({ data, sortByName, sortByScore, handleSortByName, handleS
               <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                 ABV
               </th>
-              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              <th className="whitespace-nowrap px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                 Score
                 <FontAwesomeIcon
                   icon={sortByScore === 'highToLow' ? faCaretUp : faCaretDown}
